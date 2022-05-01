0ca14504fe7a26d8003a30f540309265c138ad5c
chw081 <chw081@ucsd.edu>
2022-05-01 16:37:58 -0700

    changes to averageScore

---
+++ SkillDemo1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class SkillDemo1 {
+    ArrayList<Integer> testScores;
+
+    public SkillDemo1() {
+        testScores = new ArrayList<Integer>();
+    }
+
+    public void add(int midterm) {
+        testScores.add(midterm);
+    }
+
+    public double averageScore() {
+        int sum = 0;
+        for (int i =0; i < testScores.size(); i++) {
+            sum += testScores.get(i);
+        }
+        return (double)sum/testScores.size();
+    }
+
+    public static void main(String[] args) {
+        SkillDemo1 test = new SkillDemo1();
+        test.add(5);
+        test.add(2);
+        System.out.println(test.averageScore());
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+
+    @Test
+    public void averageScoreOdd() {
+        SkillDemo1 test = new SkillDemo1();
+        test.add(5);
+        test.add(2);
+        boolean actual = test.averageScore() == 3.5;
+        assertEquals(true, actual);
+    }
+}
+
+/*
+javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+ssh cs15lsp22adp@ieng6.ucsd.edu
+*/
